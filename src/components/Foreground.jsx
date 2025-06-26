5b3194cea3df913f000939d111ddcbf95ba8735e
akshprooo <akshprooo@gmail.com>
2025-06-26 20:57:26 +0530

    Update Foreground.jsx

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -6,9 +6,9 @@ const Foreground = () => {
   const containerRef = useRef(null);
 
   const data = [
-    { isDownload: false, progress: 0, bgcolor: '#10b981', dataAmount:'0.98 Mb', data:'making a project LOL!' }, 
+    { isDownload: true, progress: 0, bgcolor: '#10b981', dataAmount:'0.98 Mb', data:'making a project LOL!' }, 
     { isDownload: false, progress: 50, bgcolor: '#ef4444', dataAmount:'0.10 Mb', data:'making a project LOL!' }, 
-    { isDownload: false, progress: 75, bgcolor: '#8b5cf6', dataAmount:'10.5 Mb', data:'making a project LOL!' },
+    { isDownload: true, progress: 75, bgcolor: '#8b5cf6', dataAmount:'10.5 Mb', data:'making a project LOL!' },
     { isDownload: false, progress: 35, bgcolor: '#71717a', dataAmount:'0.34 Mb', data:'making a project LOL!' }, 
   ];
 
