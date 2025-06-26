62f0635d3132343e6a6e2e86fc71e7d2c2bafa71
akshprooo <akshprooo@gmail.com>
2025-06-26 20:56:59 +0530

    more dynamic things and customizations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,13 @@ import { FaRegFileAlt } from "react-icons/fa";
 import { MdDownloadForOffline } from "react-icons/md";
 import {motion, scale} from 'motion/react';
 
-const Card = (things) => {
+const Card = ({things, reference}) => {
   return (
-    <motion.div drag dragConstraints={things.reference} whileDrag={{scale: 1.2}} className='relative w-60 h-70 p-5 rounded-[20px] bg-zinc-900/90 text-white flex flex-col gap-6'>
+    <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.2}} className='relative w-60 h-80 p-5 rounded-[20px] bg-zinc-900/90 text-white flex flex-col gap-6'>
         <FaRegFileAlt />
-        <p className=' text-md leading-none font-semibold' >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, facilis.</p>
-        <div className='flex items-center px-5 absolute bottom-0 left-0 rounded-b-[20px] w-full h-15' style={{backgroundColor: !things.download ? '#025BFF' : '', justifyContent: !things.download?'center':'space-between'}}>
-            {!things.download?
+        <p className=' text-md leading-none font-semibold' >{things.data}</p>
+        <div className='flex items-center px-5 absolute bottom-0 left-0 rounded-b-[20px] w-full h-15' style={{backgroundColor: !things.isDownload ? `${things.bgcolor}` : '', justifyContent: !things.isDownload?'center':'space-between'}}>
+            {!things.isDownload?
             <>
                 <div className='w-full bg-zinc-50/50 rounded-full h-2'>
                     <div className='h-full bg-zinc-300 rounded-full' style={{width: `${things.progress}%`}}></div>
@@ -17,7 +17,7 @@ const Card = (things) => {
             </>
             :
             <>
-                <h1 className=''>0.34 Mb</h1>
+                <h1 className=''>{things.dataAmount}</h1>
                 <MdDownloadForOffline size={30}/>
             </>
             }
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -5,15 +5,24 @@ const Foreground = () => {
 
   const containerRef = useRef(null);
 
+  const data = [
+    { isDownload: false, progress: 0, bgcolor: '#10b981', dataAmount:'0.98 Mb', data:'making a project LOL!' }, 
+    { isDownload: false, progress: 50, bgcolor: '#ef4444', dataAmount:'0.10 Mb', data:'making a project LOL!' }, 
+    { isDownload: false, progress: 75, bgcolor: '#8b5cf6', dataAmount:'10.5 Mb', data:'making a project LOL!' },
+    { isDownload: false, progress: 35, bgcolor: '#71717a', dataAmount:'0.34 Mb', data:'making a project LOL!' }, 
+  ];
+
   return (
     <>
       <div ref={containerRef} className='fixed z-[3] w-full h-full top-0 left-0 flex gap-5 p-6 flex-wrap'>
-        
-        <Card download={false} progress={80} reference={containerRef} />
-        <Card download={true} progress={75} reference={containerRef} />
-        <Card download={false} progress={33} reference={containerRef} />
-        <Card download={true} progress={75} reference={containerRef} />
-
+        {
+          data.map((item,idx)=>{
+            console.log(item)
+            return(
+              <Card things={item} key={idx} reference={containerRef} />
+            )
+          })
+        }
       </div>
     </>
   )
